86d0f0f15008d5dc04ffe77131572a3fc5686412
gkucmierz <gkucmierz@gmail.com>
2017-09-19 23:55:57 +0200

    simple view

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  loading: boolean = true;
+  mode: string = 'indeterminate';
+  value: number;
+  maxCap: number = 1e7;
+  raised: number;
+
+  setPercent(percent) {
+    this.raised = percent * this.maxCap * 1e-2;
+    this.value = percent;
+  }
+
+  ngOnInit() {
+    setTimeout(() => {
+      this.loading = false;
+      this.mode = 'determinate';
+      this.setPercent(61.8034);
+    }, 1e3);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MdButtonModule, MdCheckboxModule} from '@angular/material';
+import {MdCardModule} from '@angular/material';
+import {MdProgressBarModule} from '@angular/material';
 
 import { AppComponent } from './app.component';
 
@@ -13,7 +15,9 @@ import { AppComponent } from './app.component';
     BrowserModule,
     BrowserAnimationsModule,
     MdButtonModule,
-    MdCheckboxModule
+    MdCheckboxModule,
+    MdCardModule,
+    MdProgressBarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
